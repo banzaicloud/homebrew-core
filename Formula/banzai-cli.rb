3db489355d6e31dc9583eeca3a4b64c78d9e2e62
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-22 12:30:35 +0100

    Brew formula update for banzai-cli version 0.0.5

--- Formula/banzai-cli.rb
+++ Formula/banzai-cli.rb
@@ -1,26 +1,15 @@
 class BanzaiCli < Formula
-  desc "Command-line interface for Banzai Cloud products"
+  desc "Command-line interface for Banzai Cloud Pipeline platform"
   homepage "https://banzaicloud.com/"
-  url "https://github.com/banzaicloud/banzai-cli/archive/0.0.2-preview.tar.gz"
-  sha256 "f02103fcc5073c3019fd2732530f10ff13ff2e81e23cdc8dd8b70913e5acbfad"
-
-  depends_on "dep" => :build
-  depends_on "go" => :build
+  url "https://github.com/banzaicloud/banzai-cli/releases/download/0.0.5/banzai_0.0.5_darwin_amd64.tar.gz"
+  version "0.0.5"
+  sha256 "d6c089aef53d6c158cc261023b8af8ffe731779a7743ada48df65a69cf9c423f"
 
   def install
-    ENV["GOPATH"] = buildpath
-
-    srcpath = buildpath/"src/github.com/banzaicloud/banzai-cli"
-    srcpath.install buildpath.children
-
-    cd srcpath do
-      system "dep", "ensure", "-vendor-only"
-      system "go", "build", "-o", bin/"banzai", "./cmd/banzai"
-      prefix.install_metafiles
-    end
+    bin.install "banzai"
   end
 
   test do
-    system "false"
+    system "#{bin}/banzai --version"
   end
 end
