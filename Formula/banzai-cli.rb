33230feffd8be9f3a74ea973abf77c7d515ea296
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-27 15:38:12 +0100

    Brew formula update for banzai-cli version 0.0.7

--- Formula/banzai-cli.rb
+++ Formula/banzai-cli.rb
@@ -1,9 +1,9 @@
 class BanzaiCli < Formula
   desc "Command-line interface for Banzai Cloud Pipeline platform"
   homepage "https://banzaicloud.com/"
-  url "https://github.com/banzaicloud/banzai-cli/releases/download/0.0.6/banzai_0.0.6_darwin_amd64.tar.gz"
-  version "0.0.6"
-  sha256 "4803cba546e28e0e21b4e3577082de9720723c1490c53f9d29de947891528eef"
+  url "https://github.com/banzaicloud/banzai-cli/releases/download/0.0.7/banzai_0.0.7_darwin_amd64.tar.gz"
+  version "0.0.7"
+  sha256 "5efcfc2b6738f235d8426b056a105989571e45b7818c11742c08ba464945700b"
 
   def install
     bin.install "banzai"
