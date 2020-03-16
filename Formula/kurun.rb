b60f5e1b4a86265416aa33b9527f2449ff7d5c00
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-16 14:50:18 +0100

    Brew formula update for kurun version 0.3.0

--- Formula/kurun.rb
+++ Formula/kurun.rb
@@ -2,16 +2,16 @@
 class Kurun < Formula
   desc "Run main.go in Kubernetes with one command"
   homepage "https://banzaicloud.com/blog/kurun"
-  version "0.2.5"
+  version "0.3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/banzaicloud/kurun/releases/download/0.2.5/kurun-darwin-amd64.tar.gz"
-    sha256 "de3ebd57116ded15ac6781d4e1510b888f57fd574106a1eb39f518a7297b2aab"
+    url "https://github.com/banzaicloud/kurun/releases/download/0.3.0/kurun-darwin-amd64.tar.gz"
+    sha256 "fd5879dec088e0e479a11c5a18e48415dfcc5d1271c390543280c3f9018abfe9"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/banzaicloud/kurun/releases/download/0.2.5/kurun-linux-amd64.tar.gz"
-      sha256 "bd984db39cc3280ea76320192dd896cccc9ad5de4ce2e42adb2a670501d1c01d"
+      url "https://github.com/banzaicloud/kurun/releases/download/0.3.0/kurun-linux-amd64.tar.gz"
+      sha256 "c0e9009614f38c8b59a905d02d5f626a365da8529a1168b07f0de29fa5807c74"
     end
   end
   
