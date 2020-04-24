5a258e5c95005ed38867212a2411161fc2ce836f
Peter Szabo <asdwsda@gmail.com>
2020-04-24 12:06:59 +0200

    supertubes-cli: fix url

--- Formula/supertubes-cli.rb
+++ Formula/supertubes-cli.rb
@@ -5,10 +5,10 @@ class SupertubesCli < Formula
   version "0.5.0"
 
   if OS.mac?
-    url "https://banzaicloud.com/downloads/supertubes-cli/v0.5.0/dist/supertubes_0.5.0_darwin_amd64.tar.gz"
+    url "https://banzaicloud.com/downloads/supertubes-cli/0.5.0/dist/supertubes_0.5.0_darwin_amd64.tar.gz"
     sha256 "a3a6a3ee9a46eebbaf9c5cb5def1e56e1ffd502c99f39d6560ef17a8dc7ecf08"
   elsif OS.linux?
-    url "https://banzaicloud.com/downloads/supertubes-cli/v0.5.0/dist/supertubes_0.5.0_linux_amd64.tar.gz"
+    url "https://banzaicloud.com/downloads/supertubes-cli/0.5.0/dist/supertubes_0.5.0_linux_amd64.tar.gz"
     sha256 "37b41eb9c7eff5ac72b86c54da5635989fd1aac1e93123d3aecfee6fa67bd25d"
   end
 
