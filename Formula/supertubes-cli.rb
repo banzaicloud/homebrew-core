32aa4f9367b323310b9a9768de39370d3620730b
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-05 15:06:08 +0200

    Brew formula update for supertubes-cli version v0.5.4

--- Formula/supertubes-cli.rb
+++ Formula/supertubes-cli.rb
@@ -2,14 +2,14 @@
 class SupertubesCli < Formula
   desc "Command-line interface for Supertubes"
   homepage "https://banzaicloud.com/"
-  version "0.5.3"
+  version "0.5.4"
 
   if OS.mac?
-    url "https://banzaicloud.com/downloads/supertubes-cli/0.5.3/dist/supertubes_0.5.3_darwin_amd64.tar.gz"
-    sha256 "7eac57addc190ea55fce80a1d915b9c7db20c1ab0f35435b17fa8d4ed3a69c6b"
+    url "https://banzaicloud.com/downloads/supertubes-cli/0.5.4/dist/supertubes_0.5.4_darwin_amd64.tar.gz"
+    sha256 "aa0899d26853e8ed148df6d92a7e1e8ee4b671db03ae1d1540f6c760983794a0"
   elsif OS.linux?
-    url "https://banzaicloud.com/downloads/supertubes-cli/0.5.3/dist/supertubes_0.5.3_linux_amd64.tar.gz"
-    sha256 "994ac472bb838b451d05cbb69e5c653d1fbd7f76c1ce373500a51cb3e545286f"
+    url "https://banzaicloud.com/downloads/supertubes-cli/0.5.4/dist/supertubes_0.5.4_linux_amd64.tar.gz"
+    sha256 "95cdb29545649867500a4cb445eb4155368d5f0805811e8d56158a0f6bf3b756"
   end
 
   def install
