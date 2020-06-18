c1cdcb26f3b93f14b70b9b01023bd86b11b4c848
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-18 11:05:06 +0200

    Brew formula update for one-eye version 0.2.7

--- Formula/one-eye.rb
+++ Formula/one-eye.rb
@@ -2,14 +2,14 @@
 class OneEye < Formula
   desc "Command-line interface for One Eye"
   homepage "https://banzaicloud.com/"
-  version "0.2.6"
+  version "0.2.7"
 
   if OS.mac?
-    url "https://banzaicloud.com/downloads/one-eye/0.2.6/dist/one-eye_0.2.6_darwin_amd64.tar.gz"
-    sha256 "9f044df1104f19bdec02157fe3e7971c8fc62ed534c9e14f9081410b13dcff58"
+    url "https://banzaicloud.com/downloads/one-eye/0.2.7/dist/one-eye_0.2.7_darwin_amd64.tar.gz"
+    sha256 "929c1bb55ee29ac7878e3d311ea9f502bbb4988af723a40ca6d54f8bc0ca5373"
   elsif OS.linux?
-    url "https://banzaicloud.com/downloads/one-eye/0.2.6/dist/one-eye_0.2.6_linux_amd64.tar.gz"
-    sha256 "4f001c394ebcce4f5565a5f58006e35e219c77a57ab35aeba319d8d044436123"
+    url "https://banzaicloud.com/downloads/one-eye/0.2.7/dist/one-eye_0.2.7_linux_amd64.tar.gz"
+    sha256 "9710e29811a5477086e2451723d76c59ac5cd498eaa86cfcabbf1294b25a4d9a"
   end
 
   def install
