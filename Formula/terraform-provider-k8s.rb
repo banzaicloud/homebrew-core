014c24bc61f3588a34225dae1c31354619c23355
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-25 11:45:38 +0100

    Brew formula update for terraform-provider-k8s version v0.8.4

--- Formula/terraform-provider-k8s.rb
+++ Formula/terraform-provider-k8s.rb
@@ -2,29 +2,27 @@
 class TerraformProviderK8s < Formula
   desc "Kubernetes Terraform provider with support for raw manifests"
   homepage "https://banzaicloud.com/"
-  version "0.8.3"
+  version "0.8.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/banzaicloud/terraform-provider-k8s/releases/download/v0.8.3/terraform-provider-k8s_0.8.3_darwin_amd64.zip"
-    sha256 "47d8719e1e951b303cca253405a3d9fd2142d326f935691128b35f88413e793a"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/banzaicloud/terraform-provider-k8s/releases/download/v0.8.3/terraform-provider-k8s_0.8.3_linux_amd64.zip"
-      sha256 "4acb3ad2cd5b6934f34c811b995054218327480ecca314aa886b465789723f3e"
-    end
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/banzaicloud/terraform-provider-k8s/releases/download/v0.8.3/terraform-provider-k8s_0.8.3_linux_arm64.zip"
-        sha256 "d6183549340cf41c8972d3cc5b9cdeb85c35a2bf062046a4dc4c44feaaa72f0c"
-      else
-        url "https://github.com/banzaicloud/terraform-provider-k8s/releases/download/v0.8.3/terraform-provider-k8s_0.8.3_linux_arm.zip"
-        sha256 "85fcebc420b04e2346483bd715228c65b628d119b05c7f4ad156a4b9db42409a"
-      end
-    end
+    url "https://github.com/banzaicloud/terraform-provider-k8s/releases/download/v0.8.4/terraform-provider-k8s_0.8.4_darwin_amd64.zip"
+    sha256 "ca6707acd4f409c29b63e8a09dcd8a8248ed6276ad753136d16065cbed170ba6"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/banzaicloud/terraform-provider-k8s/releases/download/v0.8.4/terraform-provider-k8s_0.8.4_linux_amd64.zip"
+    sha256 "21f61d4684ad690b567078969897d870feb811f1da74ebe9316e3693119fa99f"
+  end
+  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+    url "https://github.com/banzaicloud/terraform-provider-k8s/releases/download/v0.8.4/terraform-provider-k8s_0.8.4_linux_arm.zip"
+    sha256 "4baad3cfd1846a5ee7aaa001bc9353945418acc4407e87427b5d8294246cec00"
+  end
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/banzaicloud/terraform-provider-k8s/releases/download/v0.8.4/terraform-provider-k8s_0.8.4_linux_arm64.zip"
+    sha256 "9aa63fa1ab05772f1aa16c4f72a5673ca1a4790c7109d18b3cff6960c45ddbdb"
   end
 
   def install
-    bin.install "terraform-provider-k8s_v0.8.3"
+    bin.install "terraform-provider-k8s_v0.8.4"
   end
 end
