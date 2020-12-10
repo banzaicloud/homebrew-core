82901e8d420c6cf811d976ed49e92a1cf085ea8b
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-10 09:50:06 +0100

    Brew formula update for backyards version v1.4.8

--- Formula/backyards-cli.rb
+++ Formula/backyards-cli.rb
@@ -2,16 +2,16 @@
 class BackyardsCli < Formula
   desc "Command-line interface for Backyards"
   homepage "https://banzaicloud.com/"
-  version "1.5.0"
+  version "1.4.8"
   bottle :unneeded
 
   if OS.mac?
-    url "https://banzaicloud.com/downloads/backyards-cli/1.5.0/dist/backyards_1.5.0_darwin_amd64.tar.gz"
-    sha256 "79ce3386adce21bcebc09565a915ad09d713181df2488232eff76264bfa09738"
+    url "https://banzaicloud.com/downloads/backyards-cli/1.4.8/dist/backyards_1.4.8_darwin_amd64.tar.gz"
+    sha256 "994081bb170a02f0dfe7c257edb8e7561cab8adb5c757845d5b3a59ff7773083"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://banzaicloud.com/downloads/backyards-cli/1.5.0/dist/backyards_1.5.0_linux_amd64.tar.gz"
-      sha256 "eb5bccaa88762665896b70d5f6d1e3a6c25fb89626d942896abe2dcbd4d6d438"
+      url "https://banzaicloud.com/downloads/backyards-cli/1.4.8/dist/backyards_1.4.8_linux_amd64.tar.gz"
+      sha256 "f1a2d3390ca2c363a354bf5e5e8ff84fd6d8d58528874c8e84f52b8254bb7e2e"
     end
   end
 
