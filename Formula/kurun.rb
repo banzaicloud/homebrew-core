a7bcda17c8e6b27d603b59088c03c28c00a8c396
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-28 15:23:30 +0200

    Brew formula update for kurun version 0.5.4

--- Formula/kurun.rb
+++ Formula/kurun.rb
@@ -5,16 +5,16 @@
 class Kurun < Formula
   desc "Run main.go in Kubernetes with one command"
   homepage "https://banzaicloud.com/blog/kurun"
-  version "0.5.3"
+  version "0.5.4"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/banzaicloud/kurun/releases/download/0.5.3/kurun-darwin-amd64.tar.gz"
-    sha256 "9f093ee8cf69e43959fd34d43142607bf38bdc915c9444874d5d3c475ad0e96c"
+    url "https://github.com/banzaicloud/kurun/releases/download/0.5.4/kurun-darwin-amd64.tar.gz"
+    sha256 "639648984aa7894ca5620997e8dbac715008e71442997cfb56e7103b6cb6eecd"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/banzaicloud/kurun/releases/download/0.5.3/kurun-linux-amd64.tar.gz"
-    sha256 "a1f31d4eb7cdfd77f94fc96947d4554b365a9457791183692f3520bbd933afd5"
+    url "https://github.com/banzaicloud/kurun/releases/download/0.5.4/kurun-linux-amd64.tar.gz"
+    sha256 "7df05f731ddf15c9eb0a87f13e12d9799a6b8a371913cf0e629e117b05cfcf4d"
   end
 
   depends_on "inlets"
